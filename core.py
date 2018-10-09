7cda615ba08dec09693ca745203f4279da64917e
Iqbal Dhea F T <iko.456.ok@gmail.com>
2018-10-09 15:10:55 +0700

    oop

--- core.py
+++ core.py
@@ -7,91 +7,83 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_page(url):
-    """Get page contents from a url
-    :param url: URL to get the contents
-    :return: string page contents
-    """
-    page = requests.get(url)
-    return page.content if page.status_code == 200 else None
+class ComicScrapper:
+    def __init__(self, url, directory_name, record_file='records.txt'):
+        self.url = url
+        self.directory = 'downloads/{}'.format(directory_name.strip())
+        self.index = 1
+        self.record_file = record_file
 
+    def get_page(self):
+        """Get page contents from a url
+        :return: string page contents
+        """
+        page = requests.get(self.url)
+        return page.content if page.status_code == 200 else None
 
-def download_image(url, filename=None, directory='.', index=None):
-    """Download image from url, save it as :param filename: in :param directory:
-    """
-    filename = filename if filename is not None else url.split('/')[-1]
-    directory = directory.strip()
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-    if index:
-        filename = '{}-{}'.format(index, filename)
-    if directory.endswith('/'):
-        filename = '{}{}'.format(directory, filename)
-    else:
-        filename = '{}/{}'.format(directory, filename)
-    urlretrieve(url, filename)
-    return filename
+    def download_image(self, image_url):
+        """Download image from url
+        """
+        filename = image_url.split('/')[-1]
+        filename = '{}-{}'.format(self.index, filename)
+        full_path = '{}/{}'.format(self.directory, filename)
+        urlretrieve(image_url, full_path)
+        return filename
 
+    def record(self, rec):
+        filename = '{}/{}'.format(self.directory, self.record_file)
+        with open(filename, 'a+') as record:
+            record.write('{}\r\n'.format(rec))
 
-def record(rec, filename='records.txt', directory='.'):
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-    if directory.endswith('/'):
-        filename = '{}{}'.format(directory, filename)
-    else:
-        filename = '{}/{}'.format(directory, filename)
-    with open(filename, 'a+') as record:
-        record.write('{}\r\n'.format(rec))
+    def check_record(self):
+        result = None
+        filename = '{}/{}'.format(self.directory, self.record_file)
+        if os.path.exists(filename):
+            with open(filename, 'r') as record:
+                lines = record.readlines()
+                result = {
+                    'index': len(lines),
+                    'url': lines[-1]
+                }
+        return result
 
+    def get_next_page(self, html_page):
+        msg = ("Implement this method !\n"
+               "Make sure it return string url of next page, or None")
+        raise NotImplementedError(msg)
 
-def check_record(filename='records.txt', directory='.'):
-    result = None
-    if os.path.exists('{}/{}'.format(directory, filename)):
-        with open('{}/{}'.format(directory, filename), 'r') as record:
-            lines = record.readlines()
-            result = {
-                'index': len(lines),
-                'url': lines[-1]
-            }
-    return result
+    def get_comic_img_url(self, html_page):
+        msg = ("Implement this method !\n"
+               "Make sure it return string url of comic image")
+        raise NotImplementedError(msg)
 
-
-def run(
-        page_url='',
-        save_dir='.',
-        cont=True,
-        get_next_page=None,
-        get_comic_img_url=None,
-        verbose_level=0):
-    if get_next_page is None:
-        print('get_next_page function is not implemented')
-        return
-    if get_comic_img_url is None:
-        print('get_comic_img_url function is not implemented')
-        return
-    i = 1
-    save_dir = 'downloads/{}'.format(save_dir)
-    if cont:
-        current_record = check_record(directory=save_dir)
-        if current_record is not None:
-            i = current_record['index']+1
-            page_url = get_next_page(get_page(current_record['url']))
-    while True:
-        if not page_url:
-            break
-        page = get_page(page_url)
-        if page is None:
-            break
-        img_url = get_comic_img_url(page)
-        file_image = download_image(img_url, directory=save_dir, index=i)
-        record(page_url, directory=save_dir)
-        if verbose_level == 0:
-            print('.', end='')
-        elif verbose_level == 1:
-            print(page_url)
-        elif verbose_level == 2:
-            print('{} {}'.format(page_url, file_image))
-        page_url = get_next_page(page)
-        i += 1
-    print()
-    print('Done, result saved in {}'.format(save_dir))
+    def run(
+            self,
+            cont=True,
+            verbose_level=0):
+        if not os.path.exists(self.directory):
+                os.mkdir(self.directory)
+        if cont:
+            current_record = self.check_record()
+            if current_record is not None:
+                self.index = current_record['index']+1
+                self.url = self.get_next_page(self.get_page())
+        while True:
+            if not self.url:
+                break
+            page = self.get_page()
+            if page is None:
+                break
+            img_url = self.get_comic_img_url(page)
+            image_name = self.download_image(img_url)
+            self.record(self.url)
+            if verbose_level == 0:
+                print('.', end='')
+            elif verbose_level == 1:
+                print(self.url)
+            elif verbose_level == 2:
+                print('{} {}'.format(self.url, image_name))
+            self.url = self.get_next_page(page)
+            self.index += 1
+        print()
+        print('Done, result saved in {}'.format(self.directory))
